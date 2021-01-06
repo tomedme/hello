364760b663402651818f42c5e7811af5b1a2b03a
tom <tom@binlid.net>
2021-01-06 09:50:08 +0900

    update dependency

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module hello
 
 go 1.15
 
-require github.com/tomedme/greetings v0.0.0-20210105090723-180781350fd9
+require github.com/tomedme/greetings v0.0.0-20210106004557-c04bb5ce5a38
--- hello.go
+++ hello.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/tomedme/greetings"
 )
 
 func main() {
+	// Set properties of the predefined logger, [...]
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
 	// Get a greeting message and print it
-	message := greetings.Hello("Tom")
+	message, err := greetings.Hello("")
 	// message := "Hello Tom"
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
